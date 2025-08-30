58bd78a0271be4e3263c87f300cac5bd7919d08b
Yahya CHAKIRI <yahyachakiridev@gmail.com>
2025-08-30 14:01:32 +0100

    feat: update app/index.tsx

--- prodev-mobile-app-0x00/app-example/app/index.tsx
+++ prodev-mobile-app-0x00/app-example/app/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet, Text } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -18,9 +18,14 @@ export default function HomeScreen() {
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">First App Created</ThemedText>
-        <Text>First App Created</Text>
+        <Text>Entry Screen - Awesome</Text>
         <HelloWave />
       </ThemedView>
+      <View>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
+      </View>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
@@ -57,6 +62,29 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#90caf9",
+  },
+  largeText: {
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
+  },
+  mediumText: {
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
+  },
+  smallText: {
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',

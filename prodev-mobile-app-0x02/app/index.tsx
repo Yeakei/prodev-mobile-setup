02fc42c086d84dc03958f66b5b64c94d4226cd9a
Yahya CHAKIRI <yahyachakiridev@gmail.com>
2025-08-30 14:14:33 +0100

    fix

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -1,10 +1,7 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
